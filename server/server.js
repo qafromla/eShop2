4ad77905e23d94cee71f1a11b4574e0954a6af80
Andrew Novykov <papapin777@gmail.com>
2020-02-23 16:00:13 -0800

    Server: .env config and add middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 const express = require  ('express');
+const bodyParser =  require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
 
 const port = process.env.PORT || 3002;
 
